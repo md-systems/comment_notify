0be316d08103ed397487eb766b860a797c147c82
Arla <arild@klavaro.se>
2015-10-30 17:24:10 +0100

    Removed old permissions, menu, polished Unsubscribe form

--- src/Form/CommentNotifyUnsubscribe.php
+++ src/Form/CommentNotifyUnsubscribe.php
@@ -8,10 +8,11 @@
 namespace Drupal\comment_notify\Form;
 
 use Drupal\Core\Form\ConfigFormBase;
-use Drupal\Core\Form\FormBase;
-use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Render\Element;
 
+/**
+ * Unsubscribe form for Comment Notify.
+ */
 class CommentNotifyUnsubscribe extends ConfigFormBase {
 
   /**
@@ -31,9 +32,10 @@ class CommentNotifyUnsubscribe extends ConfigFormBase {
   public function buildForm(array $form, \Drupal\Core\Form\FormStateInterface $form_state) {
     $form['comment_notify_unsubscribe'] = [];
 
-    $form['comment_notify_unsubscribe']['email_to_unsubscribe'] = [
+    $form['comment_notify_unsubscribe']['email'] = [
       '#type' => 'textfield',
       '#title' => t('Email to unsubscribe'),
+      '#description' => $this->t('All comment notification requests associated with this email will be revoked.'),
       '#required' => TRUE,
     ];
     $form['comment_notify_unsubscribe']['submit'] = [
@@ -45,14 +47,14 @@ class CommentNotifyUnsubscribe extends ConfigFormBase {
 
   public function submitForm(array &$form, \Drupal\Core\Form\FormStateInterface $form_state) {
     module_load_include('inc', 'comment_notify', 'comment_notify');
-    $email = trim($form_state->getValue(['email_to_unsubscribe']));
+    $email = trim($form_state->getValue(['email']));
     $comments = comment_notify_unsubscribe_by_email($email);
     // Update the admin about the state of this comment notification subscription.
     if ($comments == 0) {
       drupal_set_message(t("There were no active comment notifications for that email."));
     }
     else {
-      drupal_set_message(\Drupal::translation()->formatPlural($comments, "Email unsubscribed from 1 comment notification.", "Email unsubscribed from @count comment notifications."));
+      drupal_set_message($this->formatPlural($comments, "Email unsubscribed from 1 comment notification.", "Email unsubscribed from @count comment notifications."));
     }
   }
 
